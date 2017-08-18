e448deefe05e013ed82f3364b0cbbd2fb8f241f7
Leonardo Carvalho <leonardo.carvalho@kbrtec.com.br>
2017-08-18 14:17:22 -0300

    First Commit

---
+++ Treehouse.FitnessFrog/Controllers/TesteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Treehouse.FitnessFrog.Controllers
+{
+    public class TesteController : Controller
+    {
+        // GET: Teste
+        public ActionResult Index()
+        {   
+
+
+            return View();
+        }
+    }
+}
